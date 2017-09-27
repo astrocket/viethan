1152c5334568656d703a2da875d680f1c87b067e
Astro Lee <leonardo.lee1993@gmail.com>
2017-09-27 17:07:13 +0900

    deploy for capistrano

--- app/bot/deep_search.rb
+++ app/bot/deep_search.rb
@@ -33,11 +33,11 @@ class DeepSearch < Thredded::ApplicationController
   def build_template(query , topics)
     domain = ENV["VIET_SITE_DOMAIN"] || 'localhost:3000'
     list = "#{query}에 대한 검색 결과는 다음과 같습니다."
-    unless topics
-      list << "관련된 자료를 찾을 수 없습니다. \n-----\n https://#{domain}/forum 에 들어가서 직접 질문을 올려보세요 !"
+    if topics.count == 0
+      list << "\n\n관련된 자료를 찾을 수 없습니다.\nhttps://#{domain}/forum 에 들어가서 직접 질문을 올려보세요 !"
     end
-    topics.each do |topic|
-      list << "제목 #{topic.title} \n-----\n https://#{domain}/#{topic.messageboard_path}/#{topic.title}\n---------"
+    topics.each_with_index do |topic, i|
+      list << "\n\n#{i}. #{topic.title}\nhttps://#{domain}/#{topic.messageboard_path}/#{topic.title}"
     end
     return list
   end
