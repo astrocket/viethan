617f6db53c7866836662db5529affce0be098aca
Astro Lee <leonardo.lee1993@gmail.com>
2017-10-01 10:18:17 +0900

    deploy for capistrano

--- app/bot/deep_search.rb
+++ app/bot/deep_search.rb
@@ -18,14 +18,8 @@ class DeepSearch < Thredded::ApplicationController
 
   def exclude_topics(query)
     query = query.to_s
-
-    topics = Thredded::TopicsPageView.new(
-        @current_user,
-        policy_scope(Thredded::Topic.where(messageboard_id: policy_scope(Thredded::Messageboard.all).pluck(:id)))
-            .search_query(query)
-            .order_recently_posted_first
-            .page(current_page).first(6)
-    )
+    topics = policy_scope(Thredded::Topic.where(messageboard_id: policy_scope(Thredded::Messageboard.all).pluck(:id)))
+        .search_query(query)
     return topics.to_a # Thredded::TopicsPageView's instance method
   end
 
@@ -36,13 +30,17 @@ class DeepSearch < Thredded::ApplicationController
       list << "\n\n관련된 자료를 찾을 수 없습니다.\nhttps://#{domain}/forum 에 들어가서 직접 질문을 올려보세요 !"
     end
     topics.each_with_index do |topic, i|
-      list << "\n\n#{i+1}. #{topic.title}\nhttps://#{domain}/#{topic.messageboard_path}/#{topic.title}"
+      list << "\n\n#{i+1}. #{topic.title}\nhttps://#{domain}/#{find_topic_path(topic)}/#{topic.title}"
     end
     return list
   end
 
   def current_page
     1.to_i
+  end
+
+  def find_topic_path(topic)
+    Thredded::UrlsHelper.messageboard_topics_path(topic.messageboard)
   end
 
 end
