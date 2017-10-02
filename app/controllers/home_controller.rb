eed1715470648f8801551a32e7ff5f7eae3f39b1
Astro Lee <leonardo.lee1993@gmail.com>
2017-10-03 00:54:59 +0900

    deploy for capistrano

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @posts_page_view = Thredded::PostsPageView.new(current_user, Thredded::Post.last(10).reverse!)
   end
 end
